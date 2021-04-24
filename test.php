bae41630653cdfb8b7a3a0b9d5dfd077df853fca
Kamshory, MT <kamshory@yahoo.com>
2021-04-24 14:43:57 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,38 @@
 require_once "Forex.php";
 
 $forex = new Forex('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
-echo "1 USD = ".$forex->convert('USD', 'IDR')." IDR ";
+
+?>
+<style>
+table{
+    border-collapse: collapse;
+}
+td{
+    padding: 4px 8px;
+}
+</style>
+<table border="1" style="">
+    <thead>
+        <td>From</td>
+        <td>To</td>
+        </tr>
+    </thead>
+<tbody>
+<?php
+foreach($forex->cuurencyRates as $to=>$rate)
+{
+    ?>
+            <tr>
+            <td>1 <?php echo $to;?></td>
+            <td><?php echo number_format($forex->convert($to, 'IDR'), 2, ".", "");?> IDR</td>
+            </tr>
+    <?php
+}
+?>
+</tbody>
+</table>
+<?php
+
 
 
 ?>
